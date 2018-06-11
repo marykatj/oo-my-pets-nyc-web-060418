a8c437affa76d4c9bbf6ac6333baf98c04f9811a
Mary-Kate James <marykatj@gmail.com>
2018-06-11 14:53:40 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -12,3 +12,25 @@ attr_accessor :mood
     @cat_name
   end
 end
+
+
+# Cat.ancestors   [Dog, Object, Kernel, BasicObject].  Already inheriting from other Ruby classes
+#
+# Create Pet Class file
+#
+# class Pet
+#   attr_accessor :mood
+#   attr_reader: name
+#
+#   def initialize(name)
+#     @name = name
+#     @mood = "nervous"
+#   end
+# end
+#
+#
+#
+#
+#
+#
+#
